665a1fea03b20c12b2594f1879b5357c95d34ecc
root <ody.gkount@gmail.com>
2025-02-20 12:03:45 +0200

    Debugging of the revious, "More functions and modules, more slider features"

--- your_BMI.py
+++ your_BMI.py
@@ -1,4 +1,5 @@
 import Calculations
+import human_models
 import importlib
 from IPython.display import display, Image
 
@@ -11,11 +12,11 @@ def run_bmi_calculation(height, weight):
     bmi = Calculations.calculate_bmi(height, weight)
 
     # Find the closest human model
-    human_models = Calculations.human_models_specifications()
-    closest_model = Calculations.closest_bmi(human_models, bmi)
+    human_mod = human_models.get_human_models()
+    closest_model = human_models.find_closest_model(bmi)
 
     # Get the image path
-    image_path = human_models[closest_model]["Image"]
+    image_path = human_mod[closest_model]["Image"]
 
     print(f"\nYour BMI: {bmi:.2f}")
     print(f"The closest human model to you is: {closest_model}")
